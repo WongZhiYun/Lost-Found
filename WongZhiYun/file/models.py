7b470fb66b3ef68e7fadba51763f9535e528962e
Wong Zhi Yun <zhiyun0506@gmail.com>
2025-09-07 01:34:55 +0800

    add repost post feature,posts board

--- WongZhiYun/file/models.py
+++ WongZhiYun/file/models.py
@@ -6,4 +6,16 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
     username = db.Column(db.String(150), nullable=False)
-    role = db.Column(db.String(20), default="user")   
+    role = db.Column(db.String(20), default="user")
+
+from datetime import datetime
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    type = db.Column(db.String(10), nullable=False)  # "lost" or "found"
+    category = db.Column(db.String(50), nullable=True) # optional
+    image = db.Column(db.String(100))  # filename if uploaded
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
--- WongZhiYun/file/views.py
+++ WongZhiYun/file/views.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, current_app
 from flask_login import login_required, current_user
+from .models import Post, db
+from werkzeug.utils import secure_filename
+import os
 
 views = Blueprint('views', __name__)
 
@@ -10,9 +13,51 @@ def home():
     else:
         return render_template("base.html", username=None)
     
+@views.route('/report', methods=['GET', 'POST'])
+def report_post():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        description = request.form.get('description')
+        status = request.form.get('status')
+        location = request.form.get('location')
+        date = request.form.get('date')
 
+        # handle image
+        image_file = request.files.get('image')
+        filename = None
+        if image_file:
+            filename = secure_filename(image_file.filename)
+            upload_path = os.path.join(current_app.root_path, 'static/uploads', filename)
+            image_file.save(upload_path)
 
+        # save to database
+        new_post = Post(
+            title=title,
+            description=description,
+            type=status,
+            category=location,
+            image=filename,
+            author = current_user
+        )
+        db.session.add(new_post)
+        db.session.commit()
 
+        return redirect(url_for('views.feed'))
+
+    return render_template('report.html')
+
+@views.route('/feed')
+def feed():
+    filter_type = request.args.get('type', 'all')
+    
+    if filter_type == 'lost':
+        posts = Post.query.filter_by(type='lost').order_by(Post.date_posted.desc()).all()
+    elif filter_type == 'found':
+        posts = Post.query.filter_by(type='found').order_by(Post.date_posted.desc()).all()
+    else:
+        posts = Post.query.order_by(Post.date_posted.desc()).all()
+
+    return render_template("feed.html", posts=posts, filter_type=filter_type)
 
 
 
