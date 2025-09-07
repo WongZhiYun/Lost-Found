28fee09a6115e45339510029d6d117b591893a8d
ZhiTong-06 <ztchew06@gmail.com>
2025-09-07 22:12:42 +0800

    feat(search): setup search module blueprint and template

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
 
 def create_app():
-    # craete Flask app
-    app = Flask(__name__)
+    app = Flask(__name__) # craete Flask app
 
-    app.config['SECRET_KEY'] = 'dev-key'  
-    app.config['UPLOAD_FOLDER'] = 'static/uploads'  # for AI image search
+    app.config['SECRET_KEY'] = 'dev'  
 
     # register Blueprint
     from app.search import bp as search_bp
