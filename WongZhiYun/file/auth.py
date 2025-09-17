2c92628d01152703fdef7bb1270a3ebd656b776b
Wong Zhi Yun <zhiyun0506@gmail.com>
2025-09-17 21:26:32 +0800

    setup generate otp

--- WongZhiYun/file/auth.py
+++ WongZhiYun/file/auth.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 from .models import User
 from . import db
+from .otp import generate_otp, send_otp_email
 
 auth = Blueprint('auth', __name__)
 
@@ -46,11 +47,25 @@ def sign_up():
             flash("Password field is empty!", category="error")
             return redirect(url_for("auth.sign_up"))
 
-        existing_user = User.query.filter_by(email=email).first()
-        if existing_user:
+        if User.query.filter_by(email=email).first()
             flash("Email already exists", category='error')
             return redirect(url_for('auth.sign_up'))
         
+        # --- Generate OTP ---
+        otp = generate_otp()
+        session['otp'] = otp
+        session['reg_email'] = email
+        session['reg_username'] = username
+        session['reg_password'] = generate_password_hash(password, method='pbkdf2:sha256')
+
+        # Send OTP from admin email
+        send_otp_email(email, otp)
+
+        # Reload signup page with OTP popup
+        return render_template("sign_up.html", show_otp=True)
+
+    return render_template("sign_up.html")
+
         new_user = User(
             email=email,
             username=username,
