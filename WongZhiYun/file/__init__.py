6cbdc923d01505ea2b03872434b805b26c2dbe01
Wong Zhi Yun <zhiyun0506@gmail.com>
2025-09-17 21:30:05 +0800

    changes syntax error

--- WongZhiYun/file/__init__.py
+++ WongZhiYun/file/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
-from Flask_migrate import Migrate
+from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash
 
 mail = Mail()
