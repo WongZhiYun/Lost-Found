7667b7e4b4c6cc078410ade2c5b9d12f2df6c0e1
Wong Zhi Yun <zhiyun0506@gmail.com>
2025-09-24 14:21:11 +0800

    added auth route for reset password

--- WongZhiYun/file/auth.py
+++ WongZhiYun/file/auth.py
@@ -93,3 +93,44 @@ def verify_otp():
     else:
         flash("Invalid OTP. Try again.", category='error')
         return redirect(url_for('auth.sign_up'))
+
+# 1️⃣ Request Reset Page
+@auth.route('/forgot', methods=['GET', 'POST'])
+def forgot_password():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            token = s.dumps(email, salt='reset-password')
+            link = url_for('auth.reset_password', token=token, _external=True)
+
+            msg = Message('Password Reset Request',
+                          sender='lostandfoundmmu@gmail.com',
+                          recipients=[email])
+            msg.body = f'Click the link to reset your password: {link}\n\nThis link expires in 30 minutes.'
+            mail.send(msg)
+
+            flash('Reset link sent to your email!', 'success')
+            return redirect(url_for('auth.login'))
+        else:
+            flash('No account found with that email.', 'error')
+    return render_template('forgot.html')
+
+# 2️⃣ Reset Password Page
+@auth.route('/reset/<token>', methods=['GET','POST'])
+def reset_password(token):
+    try:
+        email = s.loads(token, salt='reset-password', max_age=1800)  # 30 min expiry
+    except:
+        flash('The reset link is invalid or expired.', 'error')
+        return redirect(url_for('auth.forgot_password'))
+
+    if request.method == 'POST':
+        new_password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            user.password = generate_password_hash(new_password, method='pbkdf2:sha256')
+            db.session.commit()
+            flash('Your password has been reset!', 'success')
+            return redirect(url_for('auth.login'))
+    return render_template('reset.html')
