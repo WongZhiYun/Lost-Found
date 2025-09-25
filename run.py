ae36de610e9ab7f6c3e28384b83d045963c221bf
ZhiTong-06 <ztchew06@gmail.com>
2025-09-25 23:40:53 +0800

    delete repeating file

--- run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app # Import the app factory function from the app package
-
-app = create_app()
-
-if __name__ == '__main__':
-    # Run the Flask development server only if this script is executed directly
-    app.run(debug=True)
-    # Enable debug mode for automatic reload and detailed error page
